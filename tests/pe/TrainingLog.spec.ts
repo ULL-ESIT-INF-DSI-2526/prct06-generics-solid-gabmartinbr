fa6b154c7f98ae26fafc8a2f241b0d81119a6941
gabmartinbr <alu0101539157@ull.edu.es>
2026-03-11 12:05:38 +0000

    pe files

--- tests/pe/TrainingLog.spec.ts
+++ tests/pe/TrainingLog.spec.ts
@@ -8,17 +8,17 @@ describe("Clase Strength", () => {
     const fuerza1 = new Strength("deadlift", 100, 3, 8, 1500)
     const cardio1 = new Cardio("correr por la mañana", ActivityType.carrera, 2, 5, 2500)
 
-    it("metodos", () => {
-        log1.add(cardio1)
-        expect(log1.size).toBe(1)
-        log1.add(fuerza1)
-        expect(log1.size).toBe(2)
-        log1.remove(1)
-        expect(log1.size).toBe(1)
-        expect(log1.get(0)).toBe(cardio1)
-        expect(fuerza1.burnedCalories).toBe(1500)
+    // it("metodos", () => {
+    //     log1.add(cardio1)
+    //     expect(log1.size).toBe(1)
+    //     log1.add(fuerza1)
+    //     expect(log1.size).toBe(2)
+    //     log1.remove(1)
+    //     expect(log1.size).toBe(1)
+    //     expect(log1.get(0)).toBe(cardio1)
+    //     expect(fuerza1.burnedCalories).toBe(1500)
         
-    })
+    // })
 
     
 });
